6e96ebd2bef46c74829d108c28417cd61b6da122
admin <admin@host.docker.internal>
2021-11-12 08:08:42 +0900

    ex9no44

---
+++ ex9no44/src/ex9no44/ex9no44.java
@@ -0,0 +1,14 @@
+package ex9no44;
+
+import java.math.*;
+import static java.math.BigDecimal.*;
+import static java.math.RoundingMode.*;
+
+public class ex9no44 {  //  524p
+	public static void main(String[] args) {
+		BigDecimal bd1 = new BigDecimal("123.456");
+		BigDecimal bd2 = new BigDecimal("1.0");
+		
+		System.out.print("bd1 = " + bd1);
+	}
+}
